ab0f509de96d2b9f903e03fcb6f6aaf2a5b497b8
senthil kumar <senvyuha@gmail.com>
2025-10-14 15:26:05 +0530

    scroll to top on route change

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { Home } from './features/components/home/home';
 import { Header } from './layout/components/header/header';
 import { Footer } from './layout/components/footer/footer';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,12 @@ import { Footer } from './layout/components/footer/footer';
 })
 export class App {
   protected readonly title = signal('everi');
+
+    constructor(private router: Router){
+        this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      });
+    }
 }
